08a643ef81a243958ea61940889dacf13a22a0b5
Guilherme Alencar <programador.alencar@hotmail.com>
2023-05-23 11:39:10 -0300

    Adicionar arquivos de projeto.

---
+++ MySqlOrm.cs
@@ -0,0 +1,20 @@
+﻿using MySqlConnector;
+
+namespace MySqlOrm
+{
+    public class MySqlOrmEngine
+    {
+        private MySqlConnection _connection;
+
+        public MySqlOrmEngine()
+        {
+            
+        }
+
+        public async Task<bool> ConnectAsync(string connectionString)
+        {
+            _connection = new MySqlConnection(connectionString);
+            await _connection.OpenAsync();
+        }
+    }
+}
